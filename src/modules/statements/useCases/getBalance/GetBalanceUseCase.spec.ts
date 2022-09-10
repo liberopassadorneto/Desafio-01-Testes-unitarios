172adb85ad9b3f4d6b1f35bc0efd5c59b6d04f39
libero <liberopneto@gmail.com>
2022-09-10 15:33:41 -0400

    GET /api/v1/statements/balance

---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,117 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementUseCase } from "../createStatement/CreateStatementUseCase";
+import { ICreateStatementDTO } from "../createStatement/ICreateStatementDTO";
+import { GetBalanceError } from "./GetBalanceError";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
+let getBalanceUseCase: GetBalanceUseCase;
+
+describe("Get Balance", () => {
+  beforeEach(() => {
+    inMemoryStatementsRepository = new InMemoryStatementsRepository();
+
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+
+    getBalanceUseCase = new GetBalanceUseCase(
+      inMemoryStatementsRepository,
+      inMemoryUsersRepository
+    );
+
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+
+    createStatementUseCase = new CreateStatementUseCase(
+      inMemoryUsersRepository,
+      inMemoryStatementsRepository
+    );
+  });
+
+  it("should be able to get balance to a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+
+    const balance = await getBalanceUseCase.execute({ user_id: user.id });
+
+    expect(balance.statement).toEqual([]);
+    expect(balance.balance).toEqual(0);
+  });
+
+  it("should be able to get balance to a user with a deposit", async () => {
+    const user = await createUserUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+
+    const deposit = {
+      user_id: user.id,
+      type: "deposit",
+      amount: 100,
+      description: "Deposit",
+    } as ICreateStatementDTO;
+
+    await createStatementUseCase.execute(deposit);
+
+    const balance = await getBalanceUseCase.execute({ user_id: user.id });
+
+    expect(balance.balance).toEqual(deposit.amount);
+    expect(balance.statement.length).toEqual(1);
+    expect(balance.statement[0].amount).toEqual(deposit.amount);
+    expect(balance.statement[0].type).toEqual(deposit.type);
+    expect(balance.statement[0].description).toEqual(deposit.description);
+    expect(balance.statement[0]).toHaveProperty("id");
+  });
+
+  it("should be able to get balance to a user with a withdraw", async () => {
+    const user = await createUserUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+
+    const deposit = {
+      user_id: user.id,
+      type: "deposit",
+      amount: 100,
+      description: "Deposit",
+    } as ICreateStatementDTO;
+
+    await createStatementUseCase.execute(deposit);
+
+    const withdraw = {
+      user_id: user.id,
+      type: "withdraw",
+      amount: 100,
+      description: "Withdraw",
+    } as ICreateStatementDTO;
+
+    await createStatementUseCase.execute(withdraw);
+
+    const balance = await getBalanceUseCase.execute({ user_id: user.id });
+
+    expect(balance.balance).toEqual(deposit.amount - withdraw.amount);
+    expect(balance.statement.length).toEqual(2);
+    expect(balance.statement[0].amount).toEqual(deposit.amount);
+    expect(balance.statement[0].type).toEqual(deposit.type);
+    expect(balance.statement[0].description).toEqual(deposit.description);
+    expect(balance.statement[0]).toHaveProperty("id");
+    expect(balance.statement[1].amount).toEqual(withdraw.amount);
+    expect(balance.statement[1].type).toEqual(withdraw.type);
+    expect(balance.statement[1].description).toEqual(withdraw.description);
+    expect(balance.statement[1]).toHaveProperty("id");
+  });
+
+  it("should not be able to get balance to a non-existing user", async () => {
+    await expect(
+      getBalanceUseCase.execute({ user_id: "non-existing-user" })
+    ).rejects.toEqual(new GetBalanceError());
+  });
+});
