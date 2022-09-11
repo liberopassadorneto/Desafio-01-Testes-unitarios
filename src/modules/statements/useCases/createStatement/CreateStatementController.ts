da1a8a49505ad7562c31d9e50873ea9df500b7a8
libero <liberopneto@gmail.com>
2022-09-11 16:55:19 -0400

    Desafio 01 - Transferências com a FinAPI

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from 'express';
-import { container } from 'tsyringe';
+import { Request, Response } from "express";
+import { container } from "tsyringe";
 
-import { CreateStatementUseCase } from './CreateStatementUseCase';
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
+  DEPOSIT = "deposit",
+  WITHDRAW = "withdraw",
+  TRANSFER = "transfer",
 }
 
 export class CreateStatementController {
@@ -13,8 +14,15 @@ export class CreateStatementController {
     const { id: user_id } = request.user;
     const { amount, description } = request.body;
 
-    const splittedPath = request.originalUrl.split('/')
-    const type = splittedPath[splittedPath.length - 1] as OperationType;
+    const splittedPath = request.originalUrl.split("/");
+
+    // deposit and withdraw statements
+    let type = splittedPath[splittedPath.length - 1] as OperationType;
+
+    // transfer statement
+    if (splittedPath.length === 6) {
+      type = splittedPath[splittedPath.length - 2] as OperationType;
+    }
 
     const createStatement = container.resolve(CreateStatementUseCase);
 
@@ -22,7 +30,7 @@ export class CreateStatementController {
       user_id,
       type,
       amount,
-      description
+      description,
     });
 
     return response.status(201).json(statement);
